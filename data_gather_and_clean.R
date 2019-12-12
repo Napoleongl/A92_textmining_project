f08f6a44aef04733de1c21cb456e1a821dea65e5
napoleongl <guslu389@student.liu.se>
2019-12-12 22:36:38 +0100

    cleaning done

--- data_gather_and_clean.R
+++ data_gather_and_clean.R
@@ -1,7 +1,14 @@
 library(readr)
 library(stringr)
 library(dplyr)
+library(magrittr)
 movies <- read_csv("IMDB_Horror_movies.csv")
+
+#Cleaning Title of movies
+title_patt    <- "(^[[:alnum:]*[:space:]*[:punct:]]*)([:space:]\\([:digit:]{4}\\)?$)"
+titles        <- str_replace(string = movies$Title, pattern = title_patt, replace = "\\1")
+
+clean_movies  <- titles %>% tibble::enframe(name = NULL, value = "title")
 
 #Clean out special chars from names And handle middle name initals and Jr.:s
 plots <- str_replace_all(string = movies$Plot, pattern = "(\\b[A-Zr]{1,2})\\.", replacement = "\\1")
@@ -14,53 +21,24 @@ plot_patt     <- "([:print:]*$)"
 split_pattern <- paste0(director_patt,"[:space:]*", cast_patt, "[:space:]*", plot_patt)
 split_plots   <- str_match(pattern = split_pattern, string = plots)
 
+clean_movies  %<>% mutate(plot = split_plots[,4],
+                          director = split_plots[,2],
+                          cast  = split_plots[,3])
+
 #Indexing movies without a plot
 missing_plots <- split_plots[,4] == "" | is.na(split_plots[,4])
 
-#Cleaning Title of movies
-title_patt    <- "(^[[:alnum:]*[:space:]*[:punct:]]*)([:space:]\\([:digit:]{4}\\)?$)"
-titles        <- str_replace(string = movies$Title, pattern = title_patt, replace = "\\1")
-
 #Splitting genres into design matrix
-genres        <- str_split(movies$Genres,"\\| ", simplify = TRUE)
-genre_cats    <- as.vector(genres) %>% unique() %>% tibble() %>%
+genre_list        <- str_split(movies$Genres,"\\| ", simplify = TRUE)
+genre_cats    <- as.vector(genre_list) %>% unique() %>% tibble() %>%
     filter(!is.na(.), . != "") %>% arrange(.data =., .) %>% pull()
-genre_matrix  <- t(sapply(1:nrow(genres), function(x){
-    as.integer(genre_cats[-12] %in% genres[x,])
-    }))
-colnames(genre_matrix) <- genre_cats[-12]
+genres  <- as_tibble(t(sapply(1:nrow(genres), function(x){
+    as.integer(genre_cats[-12] %in% genre_list[x,])
+    })))
+colnames(genres) <- genre_cats[-12]
+
 #genre_commons <- genre_matrix %*% t(genre_matrix)
 
+clean_movies %<>% bind_cols(., genres) %>% filter(!missing_plots)
 
-
-
-
-
-
-u#genres <- str_replace( "[:space:]?Horror", "")
-#genres <- lapply(genres, function(x){ str_replace(x, "\\|", "") } )
-
-
-#Adapted from https://www.kaggle.com/bvarkoly/horror-movies-01-data-cleaning
-genre_columns <- sapply(1:9, function(i) { paste0("genre_", i) })
-
-library(caret)
-separated_genres <- movies %>%
-    select(Genres) %>%
-    filter(!is.na(Genres)) %>%
-    separate(col = Genres, into = genre_columns, sep = "\\|", remove = TRUE)
-    gather(key = "genre_index", value = "genre", genre_1:genre_9) %>%
-    mutate(genre = str_trim(genre)) %>%
-    mutate(genre = str_replace(genre, "-", "_")) %>%
-    filter(!is.na(genre)) %>%
-    mutate(genre_index = NULL)
-
-
-titles
-split_plots[57,]
-
-sum(!is.na(split_plots[,4]))
-
-length(unique(split_plots[,2]))
-
-
+write_csv(clean_movies, "horror_movies_cleaned.csv")
